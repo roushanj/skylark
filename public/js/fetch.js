13e38894c03a2c2c8c43395c163047e617305221
roushanj <roushan.jha0709@gmail.com>
2017-07-05 15:25:47 +0530

    modified

--- public/js/fetch.js
+++ public/js/fetch.js
@@ -1,41 +1,63 @@
-
-
- function initMap() {
-
+        function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 8,
-          center: {lat: -34.397, lng: 150.644}
+          center: {lat: 12.9716, lng: 77.5946},
+          zoom: 13
         });
-        var geocoder = new google.maps.Geocoder();
-        
-      document.getElementById('address').addEventListener('click', function() {
-          //
-          localStorage.setItem('valu', address);
-          alert(localStorage.setItem('valu', address));
+        var card = document.getElementById('pac-card');
+        var input = document.getElementById('pac-input');
+        var types = document.getElementById('type-selector');
+        var strictBounds = document.getElementById('strict-bounds-selector');
 
-          geocodeAddress(geocoder, map);
-          
+        map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
+
+        var autocomplete = new google.maps.places.Autocomplete(input);
+
+        // Bind the map's bounds (viewport) property to the autocomplete object,
+        // so that the autocomplete requests use the current map bounds for the
+        // bounds option in the request.
+        autocomplete.bindTo('bounds', map);
+
+        var infowindow = new google.maps.InfoWindow();
+        var infowindowContent = document.getElementById('infowindow-content');
+        infowindow.setContent(infowindowContent);
+        var marker = new google.maps.Marker({
+          map: map,
+          anchorPoint: new google.maps.Point(0, -29)
         });
-         
-      }
 
-      function geocodeAddress(geocoder, resultsMap) {
-  
-        var address = document.getElementById('address').value;
-  
-        geocoder.geocode({'address': address}, function(results, status) {
-          if (status === 'OK') {
-            resultsMap.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-              map: resultsMap,
-              position: results[0].geometry.location
-            });
-
-          } else {
-            alert('Geocode was not successful for the following reason: ' + status);
+        autocomplete.addListener('place_changed', function() {
+          infowindow.close();
+          marker.setVisible(false);
+          var place = autocomplete.getPlace();
+          if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            window.alert("No details available for input: '" + place.name + "'");
+            console.log('place.name');
+            return;
           }
-        });
-      }
+
+          // If the place has a geometry, then present it on a map.
+          if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+          } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);  // Why 17? Because it looks good.
+          }
+          marker.setPosition(place.geometry.location);
+          marker.setVisible(true);
+
+          var address = '';
+          if (place.address_components) {
+           
+            address = [
+              (place.address_components[0] && place.address_components[0].short_name || ''),
+              (place.address_components[1] && place.address_components[1].short_name || ''),
+              (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+          }
+          console.log(place.name);
+
 
      function loadJSON(callback) {   
       if (localStorage.getItem('v1_valu')== 'v1'){
@@ -133,24 +155,82 @@
           xobj.send(null);  
         }
     
-        function init() {
+      
               localStorage.removeItem('jsonurl')
              
              loadJSON(function(response) {
-
-               var actual_JSON = JSON.parse(response);
-
+  //                   console.log(response)
+                  
+               //var actual_JSON=JSON.parse(response);
+                
+               //console.log(country.actual_JSON[i])
                
+                            // var arr = JSON.parse(response);
 
-              for (var i = 0; i < 100; i++) {
-                  //console.log(country.actual_JSON[i])
 
-                   if (actual_JSON[i].country == 'valu') {
-                           var data = actual_JSON[i].val; 
-                           return data
-                   };
-                  };
+var arr = response
+var sorted = {};
+for (var i = 0,max = arr.length; i < max; i++) {
+    if (sorted[arr[i].country] == undefined) {
+        sorted[arr[i].country] = [];
+    }
+    sorted[arr[i].country].push(arr[i]);
+};
+var searchcountry1 =place.name;
+
+
+console.log(searchcountry1);
+
+
+var result = sorted[searchcountry1];
+console.log(result);
+console.log(result[0].country);
+console.log(result[0].measurement);
+console.log(result[0].val);
+
+             
                          
               })
 
-         }
+          infowindowContent.children['place-icon'].src = place.icon;
+          infowindowContent.children['place-name'].textContent = place.name;
+          infowindowContent.children['place-address'].textContent = address;
+         
+
+          
+
+          infowindow.open(map, marker);
+        });
+
+        // Sets a listener on a radio button to change the filter type on Places
+        // Autocomplete.
+        function setupClickListener(id, types) {
+          var radioButton = document.getElementById(id);
+          radioButton.addEventListener('click', function() {
+            autocomplete.setTypes(types);
+          });
+        }
+
+        setupClickListener('changetype-all', []);
+        setupClickListener('changetype-address', ['address']);
+        setupClickListener('changetype-establishment', ['establishment']);
+        setupClickListener('changetype-geocode', ['geocode']);
+
+        document.getElementById('use-strict-bounds')
+            .addEventListener('click', function() {
+              console.log('Checkbox clicked! New state=' + this.checked);
+              autocomplete.setOptions({strictBounds: this.checked});
+            });
+      }
+
+
+
+
+
+
+
+
+
+
+
+         
