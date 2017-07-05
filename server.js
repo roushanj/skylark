ea2faddeaa898c0086ee904f96cbc98f20cf7964
roushanj <roushan.jha0709@gmail.com>
2017-07-05 18:00:03 +0530

    new ports

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 
 var app = require('./app.js');
 
-var port = 3000;
+var port = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
