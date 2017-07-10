848ff6e826dd8016bef9be4fc41415cf359fafe7
Roushan <roushan.jha0709@gmail.com>
2017-07-10 23:01:34 +0530

    new routes

--- modules/main.js
+++ modules/main.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/main', function(req, res, next){
+router.get('/', function(req, res, next){
 	
 	res.render('main');
 });
