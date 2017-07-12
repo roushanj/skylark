03a0578a0fc5e17e7163079f5bcf8228b440335a
Roushan <roushan.jha0709@gmail.com>
2017-07-12 15:35:17 +0530

    TO display on maps

---
+++ public/js/base.js
@@ -0,0 +1,19 @@
+
+
+$( document ).ready(function() {
+    hljs.initHighlightingOnLoad();
+    $('table').addClass('table table-striped table-hover');
+});
+
+
+$('body').scrollspy({
+    target: '.bs-sidebar',
+});
+
+
+$("li.disabled a").click(function() {
+    event.preventDefault();
+});
+
+
+
